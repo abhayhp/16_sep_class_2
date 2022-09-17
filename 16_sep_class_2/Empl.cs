0fee3892b3170f0697b2274cf2ffcab39b8ab5a0
abhayhp <dadhwal.abhay.abhay@gmail.com>
2022-09-17 13:50:43 +0530

    Add project files.

---
+++ 16_sep_class_2/Empl.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_sep_class_2
+{
+    public class Empl
+    {
+        protected int id;
+        public string name;
+        public double bs, hra, da, pf, pt, total, netpaid;
+
+        public Empl(string name, double bs, double pt)
+        {
+            this.name = name;
+            this.bs = bs;
+            this.pt = pt;
+        }
+
+        public virtual void CalculateSalary()
+        {
+            hra = bs * 0.40;
+            da = bs * 0.20;
+            pf = bs * 0.12;
+            total = bs + hra + da ;
+            netpaid = total - (pf + pt);
+
+        }
+
+        public override string ToString()
+        {
+            return $"Employee name {name} , total salary {total} & Net paid {netpaid}";
+        }
+
+
+    }
+
+    public class Manager:  Empl
+    {
+        private double ta;
+        public Manager( string name, double bs, double pt, double ta) : base(name, bs, pt)
+        {
+            this.ta = ta;   
+        }
+
+        public override void CalculateSalary()
+        {
+            hra = bs * 0.40;
+            da = bs * 0.20;
+            pf = bs * 0.12;
+            total = bs + hra + da + ta;
+            netpaid = total - (pf + pt);
+
+        }
+
+        public override string ToString()
+        {
+            return $"Manager name {name} , total salary {total} & Net paid {netpaid}";
+        }
+
+
+
+
+
+    }
+
+    public class SalesManager : Empl
+    {
+        private double comm, bonus;
+
+        public SalesManager(string name, double bs, double pt, double comm, double bonus) : base(name, bs, pt)
+        {
+            this.comm = comm;
+            this.bonus = bonus;
+        }
+
+        public override void CalculateSalary()
+        {
+            hra = bs * 0.40;
+            da = bs * 0.20;
+            pf = bs * 0.12;
+            total = bs + hra + da + bonus + comm;
+            netpaid = total - (pf + pt);
+
+        }
+
+        public override string ToString()
+        {
+            return $"SalesManager name {name} , total salary {total} & Net paid {netpaid}";
+        }
+
+
+
+
+    }
+}
---
+++ 16_sep_class_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_sep_class_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Manager m1 = new Manager("A", 45000, 2000,800);
+            //m1.CalculateSalary();
+            //Console.WriteLine(m1);
+
+            //Empl e1 = new Empl("E", 40000, 1000);
+            //e1.CalculateSalary();
+            //Console.WriteLine(e1);
+
+            //Empl e2 = new Manager("M2", 45000, 2000, 800);
+            //e2.CalculateSalary();
+            //Console.WriteLine(e2);
+
+            SalesManager sm1 = new SalesManager("sm1", 46000, 400, 300, 400);
+            sm1.CalculateSalary();
+            Console.WriteLine(sm1);
+
+
+        }
+    }
+}
